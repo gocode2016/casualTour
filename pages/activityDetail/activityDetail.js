9ce2d9501627133bfa0f86282fb4c0fdd0a15d4f
duyuanli <gome123456>
2018-01-12 10:13:57 +0800

    增加活动详情页面的 咨询功能

--- pages/activityDetail/activityDetail.js
+++ pages/activityDetail/activityDetail.js
@@ -2,6 +2,7 @@
 const app = getApp()
 Page({
   data: {
+    tel: '028-88234323',
     imgUrls: [
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
@@ -12,6 +13,12 @@ Page({
     duration: 800,
     toView: 'red',
     scrollTop: 100
-    }
+  },
+  callPhone: function (e) {
+    let tel = e.currentTarget.dataset.phone;
+    wx.makePhoneCall({
+      phoneNumber: tel//仅为示例，并非真实的电话号码
+    })
   }
+}
 )
